4042b80af3d9f52c586c856a2a44e5518551633e
dora7870 <vannyzhang1@gmail.com>
2013-10-20 04:14:00 -0700

    02_filterReorder.R

--- 02_filterReorder.R
+++ 02_filterReorder.R
@@ -6,7 +6,7 @@ iDat <- read.delim("income_clean1.tsv")
 ## make a plot of agelevel with counts of income greater thann 50K
 a <- ggplot(iDat, aes(x = age, weight = cens, fill = sex)) + 
   geom_histogram(position = position_dodge(width = 0.9))
-ggsave("barchart_agecount.png")
+ggsave("barchart_ageCount.png")
 
 ## since old don't have income greater than 50K, remove the related observations of old
 iDat <- droplevels(subset(iDat, age != "Old"))
@@ -19,13 +19,13 @@ b <- ggplot(iDat, aes(x = age, y = hours.per.week, color = income)) +
   geom_jitter(alpha = 0.6, position = position_jitter(width = 0.1)) +
   scale_color_manual(values = c("#0066CC", "#FF9900"))
   facet_wrap(~ sex)
-ggsave("stripplot_agehourbysex.png")
+ggsave("stripplot_ageHourBySex.png")
 
 ## make a plot of hours per week on occupation within age
 c <- ggplot(iDat, aes(x = occupation, y = hours.per.week, color = income)) + 
   geom_jitter(alpha = 0.6, position = position_jitter(width = 0.1)) +
   facet_wrap(~ age)
-ggsave("stripplot_occuhourbyage.png")
+ggsave("stripplot_occuHourByAge.png")
 
 ## reorder data
 iDat <- arrange(iDat, sex, age, hours.per.week, income)
